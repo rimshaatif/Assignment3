2a0d0a6804564b959f5246f777a6125556f2e101
rimshaatif <rimshaatif@cmail.carleton.ca>
2024-03-18 22:15:59 -0400

    comments

--- Assignment 3 Question 1.py
+++ Assignment 3 Question 1.py
@@ -12,33 +12,54 @@ conn = psycopg2.connect(
 # Create a cursor object
 cur = conn.cursor()
 
+"""
+Retrieve all the tuples from the students table
+"""
 def getAllStudents():
-    cur.execute("SELECT * FROM students;")
+    cur.execute("SELECT * FROM students;") #SQL Query
     rows = cur.fetchall()
     
     # Print the rows
     for row in rows:
         print(row)
-        
+
+"""
+Add a student entry into the table given the firstName, lastName, email, and enrollmentDate
+"""    
 def addStudent(firstName, lastName, email, enrollmentDate):
     cur.execute("INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES (%s , %s, %s, %s)", (firstName, lastName, email, enrollmentDate))
     conn.commit()
-    
+
+"""
+Update an email address in the student table based on the studentID
+"""    
 def updateStudent(studentID, email):
-    cur.execute("UPDATE students SET email = %s WHERE student_id = %s",(email, studentID))
-    conn.commit()
-    
-def deleteStudent(studentID):
-    cur.execute("DELETE from students WHERE student_id = %s", (studentID,))
+    cur.execute("UPDATE students SET email = %s WHERE student_id = %s",(email, studentID)) #SQL Query
     conn.commit()
 
+"""
+Delete the entry of student which studentID
+""" 
+def deleteStudent(studentID):
+    cur.execute("DELETE from students WHERE student_id = %s", (studentID,)) #SQL Query
+    conn.commit()
+
+"""
+Close the connection to the Database
+"""
 def closeConnection(): 
     cur.close()
     conn.close()
 
 
-
-# getAllStudents()
-# addStudent("Test", "test", "Test.test@example.com", "2024-03-18")
-# updateStudent(4, "Test.test@gmail.com")
-# deleteStudent(5)
+###TEST METHOD CALLS######
+#getAllStudents()
+    
+#addStudent("Test", "test", "Test.test@example.com", "2024-03-18")
+#getAllStudents()
+    
+#updateStudent(4, "Test.test@gmail.com")
+#getAllStudents()
+    
+#deleteStudent(4)
+#getAllStudents()
---
+++ Assignment 3 Question 1.sql
@@ -0,0 +1,12 @@
+CREATE TABLE students (
+	student_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(250),
+    last_name VARCHAR(250),
+    email VARCHAR(250),
+    enrollment_date DATE
+);
+
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
+('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
+('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
